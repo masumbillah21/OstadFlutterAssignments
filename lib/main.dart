bcffc345e7862011f9c9919fe4530454a760d1ce
H. M. Masum Billah <mbillah21@gmail.com>
2023-09-22 22:15:19 +0600

    Assignment 07 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_projects/product_page.dart';
+import 'package:flutter_projects/product_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Assignment 06',
-      home: ProductPage(),
+      home: const ProductList(),
       theme: ThemeData(
           scrollbarTheme: ScrollbarThemeData(
         thumbColor: MaterialStateProperty.all(Colors.blue),
