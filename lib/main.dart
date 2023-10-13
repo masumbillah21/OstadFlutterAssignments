309e8ce89a542b97575f079016cc0008390a478e
H. M. Masum Billah <mbillah21@gmail.com>
2023-10-13 23:25:43 +0600

    Live Test 10 completed

--- lib/main.dart
+++ lib/main.dart
@@ -9,25 +9,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Assignment 10',
-      theme: ThemeData(
-          primarySwatch: Colors.lightBlue,
-          inputDecorationTheme: const InputDecorationTheme(
-            border: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.lightBlue,
-              ),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.lightBlue,
-              ),
-            ),
-            errorBorder: OutlineInputBorder(),
-          )),
-      home: const HomeScreen(),
+      title: 'Live Test 10',
+      home: HomeScreen(),
     );
   }
 }
